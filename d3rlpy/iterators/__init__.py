add07bae8143722b9fdd400d3d0afba042f2c4f3
Nico Gürtler <nico.guertler@tuebingen.mpg.de>
2022-08-10 19:20:48 +0200

    Import SteppedRoundIterator in in iterators.

--- d3rlpy/iterators/__init__.py
+++ d3rlpy/iterators/__init__.py
@@ -1,5 +1,7 @@
+from tkinter import W
 from .base import TransitionIterator
 from .random_iterator import RandomIterator
 from .round_iterator import RoundIterator
+from .stepped_round_iterator import SteppedRoundIterator
 
-__all__ = ["TransitionIterator", "RoundIterator", "RandomIterator"]
+__all__ = ["TransitionIterator", "RoundIterator", "RandomIterator", "SteppedRoundIterator"]
